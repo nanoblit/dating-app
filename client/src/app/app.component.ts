874b0bed127df5f872bd46d9240ff3a4b4c01480
nanoblit <kuba9519@gmail.com>
2021-04-29 08:49:04 +0200

    Add Angular.

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  title = 'The dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    // tslint:disable-next-line: deprecation
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+}
